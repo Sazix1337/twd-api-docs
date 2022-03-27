cfa32d25b775616122566a8f4c470aed73e7f756
Sazix1337 <86483949+Sazix1337@users.noreply.github.com>
2022-03-27 20:42:05 +0300

    Create api.js

---
+++ api/js/api.js
@@ -0,0 +1,18 @@
+const axios = require("axios")
+
+const execute = () => {
+    return new Promise(resolve => {
+        axios.get(`https://dev.woidzero.xyz/twaddle/api/events/listen.php?req=${btoa(JSON.stringify({'token':'u2ufYqv5BEEpSSGHWfQlh3Yi'}))}`).then(response => {
+            const r = atob(response.data)
+            const data = {
+                message: JSON.parse(r)["event"][0]["content"]["message"],
+                author: JSON.parse(r)["event"][0]["content"]["message"]["author"]
+            }
+            resolve(data)
+        }).catch(error => {
+            return error
+        })
+    })
+}
+
+module.exports = execute()
