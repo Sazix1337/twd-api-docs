f8b1bc98170b16cb4c0adc378cba85fda3269cc7
Sazix1337 <86483949+Sazix1337@users.noreply.github.com>
2022-03-30 20:16:13 +0300

    Update web-api.js

--- api/js/web-api.js
+++ api/js/web-api.js
@@ -1,1 +1,11 @@
-
+function execute_api(method_without_php, payload = Object) {
+    const xhr = new XMLHttpRequest()
+    return new Promise(resolve => {
+        xhr.open('GET', 'https://dev.woidzero.xyz/twaddle/api/' + method_without_php + '.php?req=' + btoa(JSON.stringify(payload)))
+        xhr.onload = () => {
+            const response = atob(xhr.responseText)
+            resolve(response)
+        }
+        xhr.send()
+    })
+}
